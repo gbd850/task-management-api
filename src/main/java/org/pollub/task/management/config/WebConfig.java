8d78800ae19d279f71dd31ec641989b375447f4d
gbd850 <mmyszaki@gmail.com>
2025-01-16 17:40:16 +0100

    Fix CORS policy

--- src/main/java/org/pollub/task/management/config/WebConfig.java
+++ src/main/java/org/pollub/task/management/config/WebConfig.java
@@ -1,6 +1,7 @@
 package org.pollub.task.management.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -11,6 +12,17 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.OPTIONS.name()
+                        )
+                .allowedHeaders("*")
+                .allowCredentials(true);;
     }
 }
